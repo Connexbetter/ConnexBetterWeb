1b5d4773558d227e8c9f9de1c4d82eb75387c857
anchal-github123 <anchalnayak2003@gmail.com>
2024-11-25 11:21:13 +0530

    Rcs add

--- src/Pages/Service/RCS/Rcs.jsx
+++ src/Pages/Service/RCS/Rcs.jsx
@@ -1,18 +1,18 @@
 import ContentImageBlock from "../../CommonComponents/ContentImageBlock";
 import Boxcontainer from "../../CommonComponents/Boxcontainer";
-import smsheroimg from "../../../assets/SMS-20241116T100910Z-001/SMS/Lottie/SMS Hero section.json";
 import { sms, smsProvider } from "../../../Constant";
 import LeftRightBLock from "../../CommonComponents/LeftRightBLock";
-import Lottie from "lottie-react";
-export const Sms=()=>{
+
+export const Rcs=()=>{
     return(
+
         <section>
             <div className="container">
         <ContentImageBlock
           subheading="Get Started Today"
           heading="Start sending SMS with Connex Better for free "
           content="Sign up today and create your perfect customer engagement experience."
-          ></ContentImageBlock>
+        ></ContentImageBlock>
       </div>
      
       <Boxcontainer 
@@ -36,7 +36,5 @@ export const Sms=()=>{
        
       ))}
         </section>
-
-
     )
 }
